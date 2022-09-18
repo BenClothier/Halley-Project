e2974a7fdaa76e42f7cfb7c631ca40c7d0b5824d
Ben Clothier <ben.clothier8@icloud.com>
2022-09-18 15:37:28 +0100

    Add build with tools script

---
+++ scripts/build_with_tools.bat
@@ -0,0 +1,14 @@
+set PreferredToolArchitecture=x64
+cd ..
+mkdir build
+cd build
+cmake -G "Visual Studio 15 2017 Win64" ^
+	-DHALLEY_PATH=../halley ^
+	-DBUILD_HALLEY_TOOLS=1 ^
+	-DBUILD_HALLEY_TESTS=1 ^
+	-DCMAKE_INCLUDE_PATH="lib\include;C:\dev\include;R:\dev\include" ^
+	-DCMAKE_LIBRARY_PATH="lib\windows64;C:\dev\lib64;R:\dev\lib64" ^
+	-DBOOST_ROOT="C:\dev\Boost" ^
+	-DBoost_USE_STATIC_LIBS=1 ^
+	..
+pause
