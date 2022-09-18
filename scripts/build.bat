e0c032ef453063763be65568add421242fcad5d0
Ben Clothier <ben.clothier8@icloud.com>
2022-09-18 14:20:47 +0100

    Create build script

---
+++ scripts/build.bat
@@ -0,0 +1,16 @@
+set PreferredToolArchitecture=x64
+cd ..
+mkdir build
+cd build
+cmake -G "Visual Studio 15 2017 Win64" ^
+	-DHALLEY_PATH=../halley ^
+	-DBUILD_HALLEY_TOOLS=0 ^
+	-DBUILD_HALLEY_TESTS=0 ^
+	-DCMAKE_INCLUDE_PATH="lib\include;C:\dev\include;R:\dev\include" ^
+	-DCMAKE_LIBRARY_PATH="lib\windows64;C:\dev\lib64;R:\dev\lib64" ^
+	-DBOOST_ROOT="C:\dev\Boost" ^
+	-DBoost_USE_STATIC_LIBS=1 ^
+	-SDL2_INCLUDE_DIR "C:/dev/SDL2-2.0.5/include" ^
+	-SDL2_LIBRARY "C:/dev/SDL2-2.0.5/lib/x64/SDL2.lib" ^
+	..
+pause
